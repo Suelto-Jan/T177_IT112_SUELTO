c9a01e94f56d83bc2514f780f78c6648d5562908
Suelto-Jan <115530967+Suelto-Jan@users.noreply.github.com>
2022-10-11 17:34:47 +0800

    Add files via upload

---
+++ Hello World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int num1 = 100;
+  char letter = 'A';
+  float num2 = 3.1416;
+  printf("value of  num1 is %d\n", num1);
+  printf("value of letter is %c\n", letter);
+
+}
+
